81fefd0d85cdc533ca893eb1ceed2bcf9b8236ed
Benjamin Dupuy <benjamin.dupuy8@gmail.com>
2021-05-29 14:57:15 +0200

    [ADD] Add new tiles and popups

--- src/index.js
+++ src/index.js
@@ -44,12 +44,16 @@ function initMap() {
 }
 
 function addLayers() {
-  attractions = L.layerGroup([tourMarker, canoeMarker, aviaryMarker]);
+  attractions = L.layerGroup([
+    tourMarker,
+    canoeMarker,
+    aviaryMarker,
+    beachMarker
+  ]);
   services = L.layerGroup([
     boatMarker,
     restaurantMarker,
     shopMarker,
-    beachMarker,
     homeMarker
   ]);
   infos = L.layerGroup([
@@ -76,7 +80,14 @@ function addMarkers() {
       iconAnchor: [30, 30],
       popupAnchor: [0, -30]
     })
-  }).bindPopup('Tour');
+  }).bindPopup(`
+    <div class="title">Prehistoric Tour</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-tour.jpg"></img>
+    <div class="description">Experience the night tour of the park</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button open">Open</button>
+  `);
 
   canoeMarker = L.marker([-9.96885060854611, -36.03515625000001], {
     icon: L.icon({
@@ -85,7 +96,14 @@ function addMarkers() {
       iconAnchor: [30, 30],
       popupAnchor: [0, -30]
     })
-  }).bindPopup('Canoe');
+  }).bindPopup(`
+    <div class="title">Valley of wonders</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-river.jpg"></img>
+    <div class="description">Discover an unknown world by canoe</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button closed" disabled>Closed</button>
+  `);
 
   aviaryMarker = L.marker([15.114552871944115, -59.94140625000001], {
     icon: L.icon({
@@ -94,7 +112,30 @@ function addMarkers() {
       iconAnchor: [30, 30],
       popupAnchor: [0, -30]
     })
-  }).bindPopup('Aviary');
+  }).bindPopup(`
+    <div class="title">Aviary</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-aviary.jpg"></img>
+    <div class="description">Meet the giant flying reptiles</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button closed" disabled>Closed</button>
+  `);
+
+  beachMarker = L.marker([-31.80289258670676, -7.998046875000001], {
+    icon: L.icon({
+      iconUrl: 'assets/map/map-beach.png',
+      iconSize: [60, 60],
+      iconAnchor: [30, 30],
+      popupAnchor: [0, -30]
+    })
+  }).bindPopup(`
+    <div class="title">Cretaceous Beach</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-beach.jpg"></img>
+    <div class="description">Relax on the most exotic beach</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button closed" disabled>Closed</button>
+  `);
 
   brachioMarker = L.marker([21.453068633086783, -11.25], {
     icon: L.icon({
@@ -175,7 +216,11 @@ function addMarkers() {
       iconAnchor: [20, 20],
       popupAnchor: [0, -20]
     })
-  }).bindPopup('Boat');
+  }).bindPopup(`
+    <div class="title">Ferry</div>
+    <div class="separation"></div>
+    <div class="description">Ferry landing</div>
+  `);
 
   restaurantMarker = L.marker([6.315298538330033, 3.251953125], {
     icon: L.icon({
@@ -184,7 +229,14 @@ function addMarkers() {
       iconAnchor: [20, 20],
       popupAnchor: [0, -20]
     })
-  }).bindPopup('Restaurant');
+  }).bindPopup(`
+    <div class="title">Fossile Restaurant</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-eat.jpg"></img>
+    <div class="description">Dishes from another time</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button closed" disabled>Closed</button>
+  `);
 
   shopMarker = L.marker([3.425691524418062, 6.591796875000001], {
     icon: L.icon({
@@ -193,7 +245,14 @@ function addMarkers() {
       iconAnchor: [20, 20],
       popupAnchor: [0, -20]
     })
-  }).bindPopup('Shop');
+  }).bindPopup(`
+    <div class="title">Prehistoric Domain Shop</div>
+    <div class="separation"></div>
+    <img src="/assets/pd-shop.jpg"></img>
+    <div class="description">Don't leave without a souvenir</div>
+    <div class="description">Click to enter</div>
+    <button type="button" class="button closed" disabled>Closed</button>
+  `);
 
   homeMarker = L.marker([6.664607562172573, 8.613281250000002], {
     icon: L.icon({
@@ -202,16 +261,11 @@ function addMarkers() {
       iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
       popupAnchor: [0, -20] // point from which the popup should open relative to the iconAnchor
     })
-  }).bindPopup('Home');
-
-  beachMarker = L.marker([-31.80289258670676, -7.998046875000001], {
-    icon: L.icon({
-      iconUrl: 'assets/map/map-beach.png',
-      iconSize: [40, 40],
-      iconAnchor: [20, 20],
-      popupAnchor: [0, -20]
-    })
-  }).bindPopup('Beach');
+  }).bindPopup(`
+    <div class="title">Park entrance</div>
+    <div class="separation"></div>
+    <div class="description">Your journey starts here !</div>
+  `);
 }
 
 function initMapEvents() {
